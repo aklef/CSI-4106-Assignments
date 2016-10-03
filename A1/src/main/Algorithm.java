8dbf24d8229f500a0902e118ee71f0693f8a4ec6
aklef <andreas@klef.ca>
2016-10-03 01:36:15 -0400

    Docs

--- A1/src/main/Algorithm.java
+++ A1/src/main/Algorithm.java
@@ -12,6 +12,10 @@ abstract class Algorithm
 	 * Represents a configuration (or set of configurations) of the world.
 	 */
 	protected List<Path> closedStates;
-	
+	/**
+	 * @return The computed list of Path nodes that result in the least
+	 *         expensive traversal of all piles of dirt. Return an empty list of
+	 *         Path nodes if no solution was found
+	 */
 	protected abstract List<Path> computeSolution();
 }
