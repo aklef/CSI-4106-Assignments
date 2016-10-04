1276e8b2ba480b96599f5163a49fb239411cab9b
David Alleyn <david.alleyn@gmail.com>
2016-10-04 18:48:46 -0400

    Introductory solution to the DFS algorithm.
    Finds solutions but they really suck at the moment. right right right move, right right right mov, right right right suck. etc etc

--- A1/src/main/DFS.java
+++ A1/src/main/DFS.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
+
+import main.Robot.Action;
 
 public class DFS extends Algorithm {
 
@@ -17,103 +22,153 @@ public class DFS extends Algorithm {
 
 	@Override
 	protected List<Path> computeSolution() {
-//		Robot robot = this.grid.getRobot();
-//		Path firstNode = new Path(robot, 0, grid.getDirt());
-//		Path finalNode = null;
-//		
-//		this.openStates.push(firstNode);
-//		
-//		while (!this.openStates.isEmpty() && finalNode == null)
-//		{
-//			Path node = this.openStates.pop();
-//			this.closedStates.add(node);
-//			Robot tempBot;
-//			Path newPath;
-//			
-//			if(node.remainingDirtyCells.isEmpty() )
-//			{
-//				finalNode = node;
-//				break;
-//			}
-//			
-//			for (Action action : Action.values())
-//			{
-//				tempBot = new Robot(node.roboClone);
-//				newPath = null;
-//				
-//				switch (action)
-//				{
-//					case LEFT: case RIGHT:
-//						tempBot.turn(action);
-//						newPath = new Path(node, tempBot, action, node.cost
-//								+ Action.cost(action), node.remainingDirtyCells);
-//						break;
-//					
-//					case MOVE:
-//						Position newPosition = tempBot.getCellInFrontOfRobot();
-//						Cell cellInFront;
-//						try
-//						{
-//							cellInFront = grid.getCell(newPosition);
-//							if (cellInFront.isObstructed())
-//								continue;
-//						}
-//						catch (OutOfBoundsException e)
-//						{
-//							continue;
-//						}
-//						
-//						tempBot.setPosition(newPosition);
-//						newPath = new Path(node, tempBot, action, node.cost + Action.cost(action), node.remainingDirtyCells);
-//						break;
-//					
-//					case SUCK:
-//						// WE ARE NOT ACTUALLY IMPACTING THE GRID DURING A
-//						// SEARCH // leftBot.robotClean();
-//
-//						Position cleanBotPosition = tempBot.getPosition();
-//						
-//						if (!node.remainingDirtyCells.contains(cleanBotPosition))
-//						{
-//							continue;
-//						}
-//						
-//						LinkedList<Position> newDirtyList = new LinkedList<Position>(node.remainingDirtyCells);
-//						
-//						if (!newDirtyList.remove(cleanBotPosition))
-//						{
-//							throw new RuntimeException("Tried to create a path in BFS.computeSolution() which had one less dirty tiles, but the tile to be removed was not in the dirty list");
-//						}
-//						
-//						newPath = new Path(node, tempBot, action, node.cost
-//								+ Action.cost(action), newDirtyList);
-//						break;
-//				}
-//				
-//				if(!openStates.contains(newPath)
-//						|| !closedStates.contains(newPath)	)
-//				{
-//					if(!( firstNode.roboClone.getPosition().equals(newPath.roboClone.getPosition()) //FIRST NODE SPECIAL EQUALITY CHECK, CANNOT CHECK ACTION
-//							&& firstNode.roboClone.getOrientation() == newPath.roboClone.getOrientation()  //FIRST NODE SPECIAL EQUALITY CHECK, CANNOT CHECK ACTION
-//							&& firstNode.remainingDirtyCells.equals(newPath.remainingDirtyCells)) ) //FIRST NODE SPECIAL EQUALITY CHECK, CANNOT CHECK ACTION
-//					{
-//						
-//						openStates.add(newPath);
-//						
-//					}
-//				}
-//			}
-//		}
-//		
-		LinkedList<Path> finalPathList = new LinkedList<Path>();
-//		Path finalPath = finalNode;
-//		while (finalPath != null)
-//		{
-//			finalPathList.addFirst(finalPath);
-//			finalPath = finalPath.parent;
-//		}
-//		
-//		// return null if no solution was found
-		return finalPathList;
+
+		Robot robot = this.grid.getRobot();
+		Path firstNode = new Path(robot, 0);
+		Path finalNode = null;
+		List<Path> nodesWhichSucked = new ArrayList<Path>();
+		
+		this.openStates.push(firstNode);
+		
+		while (!this.openStates.isEmpty() && finalNode == null)
+		{
+			Path node = this.openStates.pop();
+			this.closedStates.add(node);
+			Robot tempBot;
+			Path newPath;
+			
+			
+			
+			for (Action action : Action.values())
+			{
+				tempBot = new Robot(node.roboClone);
+				newPath = null;
+				
+				switch (action)
+				{
+					case LEFT: case RIGHT:
+						tempBot.turn(action);
+						newPath = new Path(node, tempBot, action, node.cost
+								+ Action.cost(action), node.getCellsAlreadyCleaned());
+						break;
+					
+					case MOVE:
+						Position newPosition = tempBot.getCellInFrontOfRobot();
+						Cell cellInFront;
+						try
+						{
+							cellInFront = grid.getCell(newPosition);
+							if (cellInFront.isObstructed())
+								continue;
+						}
+						catch (OutOfBoundsException e)
+						{
+							continue;
+						}
+						
+						tempBot.setPosition(newPosition);
+						newPath = new Path(node, tempBot, action, node.cost 
+								+ Action.cost(action), node.getCellsAlreadyCleaned());
+						break;
+					
+					case SUCK:
+						// WE ARE NOT ACTUALLY IMPACTING THE GRID DURING A
+						// SEARCH
+						
+						Position cleanBotPosition = tempBot.getPosition();
+						
+						// if
+						// (!node.remainingDirtyCells.contains(cleanBotPosition))
+						// {
+						// continue;
+						// }
+						//
+						// LinkedList<Position> newDirtyList = new
+						// LinkedList<Position>(node.remainingDirtyCells);
+						//
+						// if (!newDirtyList.remove(cleanBotPosition))
+						// {
+						// throw new
+						// RuntimeException("Tried to create a path in BFS.computeSolution() which had one less dirty tiles, but the tile to be removed was not in the dirty list");
+						// }
+						Cell cell = null;
+						try
+						{
+							cell = grid.getCell(cleanBotPosition);
+						}
+						catch (OutOfBoundsException e)
+						{
+							continue;
+						}
+						
+						if (!cell.isDirty() || node.getCellsAlreadyCleaned().contains(cell))
+						{
+							continue;
+						}
+						else
+						{
+							newPath = new Path(node, tempBot, action, node.cost
+									+ Action.cost(action), node.getCellsAlreadyCleaned());
+							nodesWhichSucked.add(newPath);
+							newPath.addCleanedCell(cell);
+						}
+						break;
+				}
+				
+				if (!(openStates.contains(newPath) || closedStates.contains(newPath)))
+				{
+					openStates.push(newPath);
+				}
+			}
+		}
+		
+		Collections.sort(nodesWhichSucked, new Comparator<Path>()
+		{
+			public int compare(Path p1, Path p2)
+			{
+				return Integer.compare(p2.getCellsAlreadyCleaned().size(), p1.getCellsAlreadyCleaned().size());
+			}
+		});
+		
+		int highestCleanCount = 0;
+		
+		Path finalPath = null;
+		if (nodesWhichSucked.isEmpty())
+		{
+			finalPath = firstNode;
+		}
+		else
+		{
+			finalPath = nodesWhichSucked.get(0);
+			highestCleanCount = finalPath.getCellsAlreadyCleaned().size();
+			
+			List<Path> maxCleanedPaths = new ArrayList<Path>();
+			
+			for (Path sucker : nodesWhichSucked)
+			{
+				if (sucker.getCellsAlreadyCleaned().size() == highestCleanCount)
+					maxCleanedPaths.add(sucker);
+			}
+			
+			Collections.sort(maxCleanedPaths, new Comparator<Path>()
+			{
+				public int compare(Path p1, Path p2)
+				{
+					return Integer.compare(p1.cost, p2.cost);
+				}
+			});
+			
+			// Re-get the least expensive node
+			finalPath = maxCleanedPaths.get(0);
+		}
+		
+		LinkedList<Path> solution = new LinkedList<Path>();
+		while (finalPath != null)
+		{
+			solution.addFirst(finalPath);
+			finalPath = finalPath.parent;
+		}
+		
+		return solution;
 	}
 }
