82e682a5d3d367241e52b75261f298564dc2b74f
aklef <andreas@klef.ca>
2016-11-10 11:58:48 -0500

    Cookie cutter stuff

---
+++ A2/robot.pl
@@ -0,0 +1,9 @@
+% Similar procedure to how it was done in A1:
+%
+% But as 'rules' in prolog:
+% Randomly choosen robot start position
+% Random orientation
+% Generated obstacles
+% Generated dirts
+
+% Implement search space nodes as coordinate pairs
