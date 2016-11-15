4b9c8d317e98c1c336978ed2169e5e7e95fc9ed4
aklef <andreas@klef.ca>
2016-11-14 19:04:10 -0500

    Delete robot.pl

--- A2/robot.pl
+++
@@ -1,9 +0,0 @@
-% Similar procedure to how it was done in A1:
-%
-% But as 'rules' in prolog:
-% Randomly choosen robot start position
-% Random orientation
-% Generated obstacles
-% Generated dirts
-
-% Implement search space nodes as coordinate pairs
