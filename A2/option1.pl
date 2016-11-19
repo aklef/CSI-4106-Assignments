4fe6fc1767d2b469fa39a054a68d0f35782d67ed
David Alleyn <david.alleyn@gmail.com>
2016-11-19 16:00:06 -0500

    Leg work.

--- A2/option1.pl
+++ A2/option1.pl
@@ -1,58 +1,115 @@
-% Similar procedure to how it was done in A1:
-%
-% Test setup is like so:
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Generating random states... Done.
-%% Robot is at pos(2, 3) facing North
-%% ╔════════╗
-%% ║░░░░░░██║
-%% ║██░░░░░░║
-%% ║░░░░¤¤↑↑║
-%% ║¤¤¤¤░░░░║
-%% ╚════════╝
-%% Search using BFS... Done.
-%% pos(row, col), Dir, action
-%% pos(2, 3), North, start
-%% pos(2, 3), West, left
-%% pos(2, 2), West, move
-%% pos(2, 2), West, suck
-%% pos(2, 1), West, move
-%% pos(2, 1), South, left
-%% pos(3, 1), South, move
-%% pos(3, 1), South, suck
-%% pos(3, 1), West, right
-%% pos(3, 0), West, move
-%% pos(3, 0), West, suck
-%% total cost: 290
-%% Depth: 11
-%% Time : 68 ms
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% But as 'rules' in prolog:
-% Randomly choosen robot start position. ex:
-startPos(1, 0).
-% Random orientation. ex:
-startOri(South).
-% Generated obstacles
+% CLIMATES
 
-% Generated dirts
-% dirt(x, y)
-dirt(0, 2).
-dirt(1, 2).
-dirt(0, 2).
+% This is a sample of a classification expert system for identification
+% of Koppen climate types. The rules are roughly based on the classifications
+% from https://en.wikipedia.org/wiki/K%C3%B6ppen_climate_classification
 
-neighbour(X1, X2, Y1,Y2):-
-    horizontalNeighbour(X1, X2, Y1,Y2),
-    verticalNeighbour(X1, X2, Y1,Y2).
+% This type of expert system can easily use Prolog's built in inferencing
+% system. While trying to satisfy the goal "cat" it tries to satisfy
+% various subgoals, some of which will ask for information from the
+% user.
 
-horizontalNeighbour(X1, X2, Y1,Y2):-
-    dirt(X1,Y1),
-    dirt(X2,Y2),
-    /* dont forget to check that 'X' is not equal to 'Y'*/
-    X1\=X2.
+% The information is all stored as attribute-value pairs. The attribute
+% is represented as a predicate, and the value as the argument to the
+% predicate. For example, the attribute-value pair "color-brown" is
+% stored "color(brown)".
 
-verticalNeighbour(X1, X2, Y1,Y2):-
-    dirt(X1,Y1),
-    dirt(X2,Y2),
-    /* dont forget to check that 'X' is not equal to 'Y'*/
-    X1\=X2.
-% Implement search space nodes as coordinate pairs
+% "identify" is the high level goal that starts the program. The
+% predicate "known/3" is used to remember answers to questions, so it
+% is cleared at the beginning of the run.
+
+% The rules of identification are the bulk of the code. They break up
+% the problem into identifying process and ingredients and tools before identifying
+% the actual climates.
+
+% The end of the code lists those attribute-value pairs which need
+% to be asked for, and defines the predicate "ask" and "menuask"
+% which are used to get information from the user, and remember it.
+
+
+main :- identify.
+
+identify:-
+  retractall(known(_,_,_)),         % clear stored information
+  cat(X),
+  write('The climate classification is '),write(X),nl.
+identify:-
+  write('I can''t identify that climate classification'),nl.
+
+
+
+% "ask" is responsible for getting information from the user, and remembering
+% the users response. If it doesn't already know the answer to a question
+% it will ask the user. It then asserts the answer. It recognizes two
+% cases of knowledge: 1) the attribute-value is known to be true,
+% 2) the attribute-value is known to be false.
+
+% This means an attribute might have multiple values. A third test to
+% see if the attribute has another value could be used to enforce
+% single valued attributes. (This test is commented out below)
+
+% For this system the menuask is used for attributes which are single
+% valued
+
+% "ask" only deals with simple yes or no answers. a "yes" is the only
+% yes value. any other response is considered a "no".
+
+ask(Attribute,Value):-
+  known(yes,Attribute,Value),       % succeed if we know its true
+  !.                                % and dont look any further
+ask(Attribute,Value):-
+  known(_,Attribute,Value),         % fail if we know its false
+  !, fail.
+
+ask(Attribute,_):-
+  known(yes,Attribute,_),           % fail if we know its some other value.
+  !, fail.                          % the cut in clause #1 ensures that if
+                                    % we get here the value is wrong.
+ask(A,V):-
+  write(A:V),                       % if we get here, we need to ask.
+  write('? (yes or no): '),
+  read(Y),                          % get the answer
+  asserta(known(Y,A,V)),            % remember it so we dont ask again.
+  Y = yes.                          % succeed or fail based on answer.
+
+% "menuask" is like ask, only it gives the user a menu to to choose
+% from rather than a yes on no answer. In this case there is no
+% need to check for a negative since "menuask" ensures there will
+% be some positive answer.
+
+menuask(Attribute,Value,_):-
+  known(yes,Attribute,Value),       % succeed if we know
+  !.
+menuask(Attribute,_,_):-
+  known(yes,Attribute,_),           % fail if its some other value
+  !, fail.
+
+menuask(Attribute,AskValue,Menu):-
+  nl,write('What is the value for '),write(Attribute),write('?'),nl,
+  display_menu(Menu),
+  write('Enter the number of choice> '),
+  read(Num),nl,
+  pick_menu(Num,AnswerValue,Menu),
+  asserta(known(yes,Attribute,AnswerValue)),
+  AskValue = AnswerValue.           % succeed or fail based on answer
+
+display_menu(Menu):-
+  disp_menu(1,Menu), !.             % make sure we fail on backtracking
+
+disp_menu(_,[]).
+disp_menu(N,[Item | Rest]):-        % recursively write the head of
+  write(N),write(' : '),write(Item),nl, % the list and disp_menu the tail
+  NN is N + 1,
+  disp_menu(NN,Rest).
+
+pick_menu(N,Val,Menu):-
+  integer(N),                       % make sure they gave a number
+  pic_menu(1,N,Val,Menu), !.        % start at one
+  pick_menu(Val,Val,_).             % if they didn't enter a number, use
+                                    % what they entered as the value
+
+pic_menu(_,_,none_of_the_above,[]). % if we've exhausted the list
+pic_menu(N,N, Item, [Item|_]).      % the counter matches the number
+pic_menu(Ctr,N, Val, [_|Rest]):-
+  NextCtr is Ctr + 1,               % try the next one
+  pic_menu(NextCtr, N, Val, Rest).
